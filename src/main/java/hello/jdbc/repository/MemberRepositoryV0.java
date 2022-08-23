3b9dd462d7a7593b72782373db476619dc82b630
sunghoonkim <shkim11997@gmail.com>
2022-08-23 20:48:46 +0900

    JDBC 개발 -조회- select 쿼리

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -1,6 +1,8 @@
 package hello.jdbc.repository;
 
 
+import static hello.jdbc.connection.DBConnectionUtil.getConnection;
+
 import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import java.sql.Connection;
@@ -8,10 +10,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- *  JDBC - DriverManager 사용
+ * JDBC - DriverManager 사용
  */
 @Slf4j
 public class MemberRepositoryV0 {
@@ -44,13 +47,42 @@ public class MemberRepositoryV0 {
 
     }
 
+    public Member findById(String memberId) throws SQLException {
+
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+
+            close(con, pstmt, rs);
+        }
+    }
+
     private void close(Connection con, Statement stmt, ResultSet rs) {
         if (rs != null) {
             try {
                 rs.close();
             } catch (SQLException e) {
                 log.info("error", e);
-            } }
+            }
+        }
         if (stmt != null) {
             try {
                 stmt.close();
@@ -64,8 +96,11 @@ public class MemberRepositoryV0 {
             } catch (SQLException e) {
                 log.info("error", e);
             }
-        } }
-    private Connection getConnection() {
-        return DBConnectionUtil.getConnection();
+        }
     }
+
+//    private Connection getConnection() {
+//        return getConnection();
+//    }
+
 }
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,24 @@
 package hello.jdbc.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 import hello.jdbc.domain.Member;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
     void crud() throws SQLException {
-//save
-        Member member = new Member("memberV01", 10000);
+        //save
+        Member member = new Member("memberV0", 10000);
         repository.save(member);
-    }
-}
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+    } }
